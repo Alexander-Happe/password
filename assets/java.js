fcd4cd09479163f29e66aba365c25520d26e8a39
Alex Happe <abhdiver1994@gmail.com>
2019-10-25 22:31:10 -0500

    adds functionality to the checkers so that they re prompt the user

--- assets/java.js
+++ assets/java.js
@@ -6,12 +6,12 @@ console.log (clipbutton)
 
 function lengthcheck(a){
     if (a < 8 || a > 128) {
-        alert("Must be between 8-128 characters!")
+        return true
     }
 }
 function charctercheck(a, b, c, d) {
     if (a === false && b === false && c === false && d === false) {
-        alert("Must choose at least one option.")
+        return true
     }
 }
 passbutton.addEventListener("click", function() {
@@ -22,6 +22,15 @@ passbutton.addEventListener("click", function() {
     special = confirm("Would you like special characters?")
     
 
-    lengthcheck(length)
-    charctercheck(uppercase, lowercase, num, special)
+    while (lengthcheck(length) === true) {
+        alert("Must be between 8-128 characters!")
+        length = prompt("Pick number of characters. (8-128")
+    }
+    while (charctercheck(uppercase, lowercase, num, special) === true){
+        alert("Must choose at least one option!")
+        uppercase = confirm("Would you like upper case letters?")
+        lowercase = confirm("Would you like lower case letters?")
+        num = confirm("Would you like numbers?")
+        special = confirm("Would you like special characters?")
+    }
 })
