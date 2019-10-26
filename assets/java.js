bf8e487ca7f72950f19128ac404ed5383f4b5e1b
Alex Happe <abhdiver1994@gmail.com>
2019-10-25 23:12:39 -0500

    simplifies the check

--- assets/java.js
+++ assets/java.js
@@ -4,16 +4,6 @@ var clipbutton = document.querySelector("#clipbutton")
 console.log (passbutton)
 console.log (clipbutton)
 
-function lengthcheck(a){
-    if (a < 8 || a > 128) {
-        return true
-    }
-}
-function charctercheck(a, b, c, d) {
-    if (a === false && b === false && c === false && d === false) {
-        return true
-    }
-}
 passbutton.addEventListener("click", function() {
     length = prompt("Pick number of characters. (8-128")
     uppercase = confirm("Would you like upper case letters?")
@@ -21,16 +11,18 @@ passbutton.addEventListener("click", function() {
     num = confirm("Would you like numbers?")
     special = confirm("Would you like special characters?")
     
-
-    while (lengthcheck(length) === true) {
+    while (length < 8 || length > 128) {
         alert("Must be between 8-128 characters!")
         length = prompt("Pick number of characters. (8-128")
     }
-    while (charctercheck(uppercase, lowercase, num, special) === true){
+
+    
+    while (uppercase === false && lowercase === false && num === false && special === false){
         alert("Must choose at least one option!")
         uppercase = confirm("Would you like upper case letters?")
         lowercase = confirm("Would you like lower case letters?")
         num = confirm("Would you like numbers?")
         special = confirm("Would you like special characters?")
     }
+    
 })
