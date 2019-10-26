d9b6e6c8262f8a5f25699dc3bc742c6bec139b09
Alex Happe <abhdiver1994@gmail.com>
2019-10-26 00:01:37 -0500

    adds password generation

--- assets/java.js
+++ assets/java.js
@@ -1,17 +1,19 @@
 var passbutton = document.querySelector("#passbutton")
 var clipbutton = document.querySelector("#clipbutton")
-
+var text = document.querySelector(".textbox")
 console.log (passbutton)
 console.log (clipbutton)
-
+console.log (text)
 passbutton.addEventListener("click", function() {
     length = prompt("Pick number of characters. (8-128")
     uppercase = confirm("Would you like upper case letters?")
     lowercase = confirm("Would you like lower case letters?")
     num = confirm("Would you like numbers?")
     special = confirm("Would you like special characters?")
-    
-    while (length < 8 || length > 128) {
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?*$@%#"
+    password = ""
+
+    while (length < 8 || length > 128 || /[0-9]+$/.test(length) != true) {
         alert("Must be between 8-128 characters!")
         length = prompt("Pick number of characters. (8-128")
     }
@@ -24,5 +26,14 @@ passbutton.addEventListener("click", function() {
         num = confirm("Would you like numbers?")
         special = confirm("Would you like special characters?")
     }
+
+    if (uppercase === true && lowercase === true && num === true && special === true){
+        for (i = 0, n = charset.length; i < length; i++){
+            password += charset.charAt(Math.random() * n)
+            
+        }
+    }
+    text.textContent = password
+    
     
 })
