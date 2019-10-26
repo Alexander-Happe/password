8a983c783fdac9ae3bc82c933fd95197fb58b045
Alex Happe <abhdiver1994@gmail.com>
2019-10-26 12:39:03 -0500

    finishes the project

--- assets/java.js
+++ assets/java.js
@@ -1,16 +1,14 @@
 var passbutton = document.querySelector("#passbutton")
 var clipbutton = document.querySelector("#clipbutton")
 var text = document.querySelector(".textbox")
-console.log (passbutton)
-console.log (clipbutton)
-console.log (text)
+
 passbutton.addEventListener("click", function() {
     length = prompt("Pick number of characters. (8-128")
     uppercase = confirm("Would you like upper case letters?")
     lowercase = confirm("Would you like lower case letters?")
     num = confirm("Would you like numbers?")
     special = confirm("Would you like special characters?")
-    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?*$@%#"
+    charset = ""
     password = ""
 
     while (length < 8 || length > 128 || /[0-9]+$/.test(length) != true) {
@@ -19,7 +17,7 @@ passbutton.addEventListener("click", function() {
     }
 
     
-    while (uppercase === false && lowercase === false && num === false && special === false){
+    while (!uppercase && !lowercase && !num && !special){
         alert("Must choose at least one option!")
         uppercase = confirm("Would you like upper case letters?")
         lowercase = confirm("Would you like lower case letters?")
@@ -27,13 +25,36 @@ passbutton.addEventListener("click", function() {
         special = confirm("Would you like special characters?")
     }
 
-    if (uppercase === true && lowercase === true && num === true && special === true){
-        for (i = 0, n = charset.length; i < length; i++){
-            password += charset.charAt(Math.random() * n)
-            
-        }
+    if (uppercase) {
+        charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     }
+    if (lowercase){
+        charset += "abcdefghijklmnopqrstuvwxyz"
+    }
+    if (num) {
+        charset += "0123456789"
+    }
+    if (special) {
+        charset += "!@#$%&*?"
+    }
+    
+    for (i = 0, n = charset.length; i < length; i++){
+        password += charset.charAt(Math.random() * n)
+            
+    }
+    
+
     text.textContent = password
-    
-    
+})
+
+clipbutton.addEventListener("click", function(){
+    el = document.createElement('textarea');
+    el.value = password;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+    alert("Copied password: " + el.value)
+
+
 })
